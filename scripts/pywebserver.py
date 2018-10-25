5fd5550ca9027b1e2778f48f747727123c37b74d
Akkharaphong EKSIRI <akkharaphong.e@bu.ac.th>
2018-10-25 12:46:39 +0700

    Add files via upload

---
+++ scripts/pywebserver.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import SimpleHTTPServer
+import SocketServer
+import socket
+import os
+from rospkg import RosPack
+
+rp = RosPack()
+frontend_path = rp.get_path('turtlebot3_blockly')
+frontend_path += '/frontend'
+
+print("Changing serve path to: " + frontend_path)
+
+os.chdir(frontend_path)
+
+HOST = socket.gethostname()
+PORT = 1036
+address = ("",PORT)
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(address, Handler)
+
+print("serving at port", PORT)
+httpd.serve_forever()
